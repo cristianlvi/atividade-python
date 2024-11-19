d0098ba34db903a28e9f18e6d5d9e03a8b1e5a7c
Cristian Luan Vieira <cristian.luanvieira@gmail.com>
2024-11-19 00:30:51 -0300

    refactor: add nova variavel com valor da divisao

--- atividade1.py
+++ atividade1.py
@@ -8,23 +8,24 @@
 print('Bem-vindo ao sistema do Cristian')
 valorBase = float(input('Digite o valor base do plano: '))
 idade = int(input('Digite a idade do cliente: ')) 
+divisao = 100
 
 if idade >= 0 and idade < 19:
     print(f'O valor mensal do plano é de R${valorBase}')
 elif idade >= 19 and idade < 29:
-    valorMensal = valorBase * (150 / valorBase)
+    valorMensal = valorBase * (150 / divisao)
     print(f'O valor mensal do plano é de R${valorMensal}')
 elif idade >= 29 and idade < 39:
-    valorMensal = valorBase * (225 / valorBase)
+    valorMensal = valorBase * (225 / divisao)
     print(f'O valor mensal do plano é de R${valorMensal}')
 elif idade >= 39 and idade < 49:
-    valorMensal = valorBase * (240 / valorBase)
+    valorMensal = valorBase * (240 / divisao)
     print(f'O valor mensal do plano é de R${valorMensal}')
 elif idade >= 49 and idade < 59:
-    valorMensal = valorBase * (350 / valorBase)
+    valorMensal = valorBase * (350 / divisao)
     print(f'O valor mensal do plano é de R${valorMensal}')
 else:
-    valorMensal = valorBase * (600 / valorBase)
+    valorMensal = valorBase * (600 / divisao)
     print(f'O valor mensal do plano é de R${valorMensal}')
     
     
